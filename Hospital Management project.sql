65e04f36eedc10dc5038ff43daefd19ef4d49d8b
Kamalesh <67744300+Kamalesh242@users.noreply.github.com>
2024-10-16 21:38:32 +0530

    Update Hospital Management project.sql

--- Hospital Management project.sql
+++ Hospital Management project.sql
@@ -10,7 +10,7 @@
 		 
 3. Write a SQL query to join both the tables using inner join?
 	     
-		 select
+   select
             e.doctor_name,
             e.doctorid,
             e.speciality,
@@ -23,7 +23,7 @@
 	 
 4. Write a SQL query to join both the tables using right join?
 	 
-	       select
+   select
             e.doctor_name,
             e.doctorid,
             e.speciality,
@@ -46,7 +46,7 @@
 		  
 6. Write a SQL query to join both the tables using outer join?
 	 
-	       select
+   select
             e.doctor_name,
             e.doctorid,
             e.speciality,
@@ -54,6 +54,7 @@
           from doctors e
           full outer join patients d
           ON e.doctorid = d.doctorid;
+
 	
  7.  Write an sql query to find nth highest salary in doctors table
 		 
@@ -64,42 +65,45 @@
 		  
 8.  Write an sql query to find odd and even rows of doctorid in doctors table??
 		 
-	      select * from doctors where mod(doctorid,2) = 0;  ---even
+    select * from doctors where mod(doctorid,2) = 0;  ---even
 		  
-		  select * from doctors where mod(doctorid,2) = 0;  ---odd
+    select * from doctors where mod(doctorid,2) = 0;  ---odd
 		 
 		 
 9. Display all the contents in doctors table ??
         
-		select * from doctors;
+   select * from doctors;
+
 
 10. Display the data stucture of doctors table ??
        
-		desc doctors;
+    desc doctors;
+
 11. Count the number of male and female present in doctor_gender column
         
-	SELECT doctor_gender, COUNT(*) AS total_count
-        FROM doctors
-        GROUP BY doctor_gender;
+    SELECT doctor_gender, COUNT(*) AS total_count
+    FROM doctors
+    GROUP BY doctor_gender;
 
 12. Calculate the total rows count in doctors table ??
         
-		select count(*) as total_count
-        from doctors;
+    select count(*) as total_count
+    from doctors;
 		
 13. Display the salary column from doctors table without duplicates
 	 
-        select distinct salary from doctors
-        order by salary asc;
+    select distinct salary from doctors
+    order by salary asc;
 
-     14. Select the doctorid, doctor_name, speciality of doctors with gender female
+14. Select the doctorid, doctor_name, speciality of doctors with gender female
         
-		select doctorid, doctor_name, speciality from doctors
-        where doctors_gender = 'f';
+    select doctorid, doctor_name, speciality from doctors
+    where doctors_gender = 'f';
 
-     15. List the salary from doctors table which are greater than 100000
-         select salary from doctors
-         where salary > 100000;
+15. List the salary from doctors table which are greater than 100000
+	
+    select salary from doctors
+    where salary > 100000;
 
 16. Select the doctorid, doctor_name of those whose speciality is cardiology, neurology, pediatrics ??
 	 
@@ -108,52 +112,51 @@
 
 17. Count of female and male doctors having salary less than 1 lakh
          
-		 select doctor_gender, count(*) as totaldoctors
+    select doctor_gender, count(*) as totaldoctors
          from doctors
          where salary < 100000
          group by doctor_gender;
 		  
 18. List the max, min , avg salary from doctors table ??
          
-		 SELECT MAX(salary) AS max_salary
-         FROM doctors;
+    SELECT MAX(salary) AS max_salary
+    FROM doctors;
 
 
-		 SELECT MIN(salary) AS min_salary
-         FROM doctors;
+    SELECT MIN(salary) AS min_salary
+    FROM doctors;
 		 
-		 SELECT AVG(salary) AS avg_salary
-         FROM doctors;
+    SELECT AVG(salary) AS avg_salary
+    FROM doctors;
 		 
 		 
 19. List the second highest salary from doctors table ??
          
-		 SELECT MAX(salary) AS second_highest_salary
-         FROM doctors
-         WHERE salary < (SELECT MAX(salary) FROM doctors);
+    SELECT MAX(salary) AS second_highest_salary
+    FROM doctors
+    WHERE salary < (SELECT MAX(salary) FROM doctors);
 		 
 		 
 20. SQL query to find total salary department wise
 	     
-		 select speciality,sum(salary) as total salary
-		 from doctors
-		 group by speciality;
+    select speciality,sum(salary) as total salary
+    from doctors
+    group by speciality;
 	 
  21. Display the data stucture of doctors table ??
         
-		 desc doctors;
+     desc doctors;
 		 
  22. SQL query to create a table with the same structure with or Without data of the Existing table
 	 
-	      create table doctors_details as select * from doctors  ---same data
+     create table doctors_details as select * from doctors  ---same data
 		  
-		   create table doctors_details as select * from doctors whwre A = B ;  ---without data
+     create table doctors_details as select * from doctors whwre A = B ;  ---without data
 
 
-
-     23. SQL query to find duplicates in speaciality??
+ 23. SQL query to find duplicates in speaciality??
 	      
-		  SELECT speciality, COUNT(*) AS DuplicateCount
-          FROM doctors
-          GROUP BY speciality
-          HAVING COUNT(*) > 1;
+     SELECT speciality, COUNT(*) AS DuplicateCount
+     FROM doctors
+     GROUP BY speciality
+     HAVING COUNT(*) > 1;
